4f75416ab5f57d02da249040b072cc2f9e70b354
askar <mr.askar1984@gmail.com>
2022-11-11 21:36:43 +0200

    sd

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -38,7 +38,8 @@
                     <div class="sidebar-toggle-box">
                         <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
                         <!--logo start-->
-                        <a href="" class="logo bold">F O D A <i class="fa fa-bullhorn"></i><span> </span></a>
+                        <i class="fa fa-bullhorn">
+                        <a href="" class="logo bold">F O D A </i><span> </span></a>
                     </div>
                     <!--logo end-->
                 </div>
