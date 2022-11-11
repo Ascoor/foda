3873047ab1652936ea9e19f231d710cd9fb77ee6
askar <mr.askar1984@gmail.com>
2022-11-11 21:40:11 +0200

    sd

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -36,13 +36,13 @@
             <header class="header white-bg">
                 <div class="col-md-2 logo_bac">
                     <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
+                    <div class="nav notify-row" id="top_menu">
                 </div>
                 <!--logo start-->
                 <div class="sidebar-toggle-box">
                 <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
                 <!--logo end-->
                 </div>
-                <div class="nav notify-row" id="top_menu">
                     <!--  notification start -->
                     <ul class="nav top-menu">
                     </ul>
