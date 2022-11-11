a7e90882dfcb8478e50144309161c58e0b64450f
askar <mr.askar1984@gmail.com>
2022-11-11 03:14:32 +0200

    js

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ar" dir="rtl">
     <head>
         <base href="<?php echo base_url(); ?>">
         <meta charset="utf-8">
@@ -10,7 +10,9 @@
         <link rel="shortcut icon" href="uploads/favicon.png">
         <title><?php echo $this->router->fetch_class(); ?> | Election Campaign Management System</title>
         <!-- Bootstrap core CSS -->
-        <link href="common/css/bootstrap.min.css" rel="stylesheet">
+   <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
         <link href="common/css/bootstrap-reset.css" rel="stylesheet">
         <!--external css-->
         <link href="common/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
