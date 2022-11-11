a2d849804e92ebdf4a49c4c0f7f7a06b1b8f9c0f
askar <mr.askar1984@gmail.com>
2022-11-11 21:23:47 +0200

    df

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -38,8 +38,9 @@
                     <div class="sidebar-toggle-box">
                         <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
                         <!--logo start-->
-                        <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
+                        <a href="" class="logo bold">F O D A 2025 <i class="fa fa-bullhorn"></i><span> </span></a>
                         <!--logo end-->
+                    </div>
                     </div>
                 <div class="nav notify-row" id="top_menu">
                     <!--  notification start -->
