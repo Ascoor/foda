2d69a50ade57e6698883ab010caadc1e8fde0ee9
askar <mr.askar1984@gmail.com>
2022-11-11 03:49:56 +0200

    fsd

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -57,7 +57,7 @@
               
              
                 <div class="sidebar-toggle-box">
-                        <div data-original-title="Toggle Navigation" data-placement="left" class="fa fa-bars tooltips"></div>
+                        <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
                     </div>
                     <!--logo start-->
                     <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
