dc528636a233181888e93d0fb0988cdb4dca01d6
askar <mr.askar1984@gmail.com>
2022-11-11 03:18:04 +0200

    d

--- application/modules/home/views/footer.php
+++ application/modules/home/views/footer.php
@@ -14,7 +14,7 @@
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="common/js/jquery.scrollTo.min.js"></script>
+<!-- <script src="common/js/jquery.scrollTo.min.js"></script>
 <script src="common/js/jquery.nicescroll.js" type="text/javascript"></script>
 <script type="text/javascript" src="common/assets/data-tables/jquery.dataTables.js"></script>
 <script type="text/javascript" src="common/assets/data-tables/DT_bootstrap.js"></script>
@@ -24,7 +24,7 @@
 
 
 <script type="text/javascript" src="common/assets/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-<script src="common/js/advanced-form-components.js"></script>
+<script src="common/js/advanced-form-components.js"></script> -->
 
 <script type="text/javascript" src="common/assets/ckeditor/ckeditor.js"></script>
 
