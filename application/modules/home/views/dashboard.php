e1a23540535fcecad0f28097f2ee1099d8838360
askar <mr.askar1984@gmail.com>
2022-11-11 21:39:06 +0200

    sd

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -35,12 +35,12 @@
             <!--header start-->
             <header class="header white-bg">
                 <div class="col-md-2 logo_bac">
-                    <div class="sidebar-toggle-box">
-                        <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
-                    </div>
-                    <!--logo start-->
-                    <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
-                    <!--logo end-->
+                    <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
+                </div>
+                <!--logo start-->
+                <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
+                <!--logo end-->
+                <div class="sidebar-toggle-box">
                 </div>
                 <div class="nav notify-row" id="top_menu">
                     <!--  notification start -->
