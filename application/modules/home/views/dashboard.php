c538be841d5ed3be580b6af0071620c924295b1a
askar <mr.askar1984@gmail.com>
2022-11-11 04:14:02 +0200

    asd

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -36,10 +36,11 @@
         <section id="container" class="">
             <!--header start-->
             <header class="header white-bg">
-          
                 <div class="nav notify-row" id="top_menu">
                     <!--  notification start -->
-                    
+                    <ul class="nav top-menu">
+                        </ul>
+                    </div>
                     <div class="top-nav ">
                     <?php
                     $message = $this->session->flashdata('feedback');
@@ -54,18 +55,16 @@
                             <a href="profile" class="allright"> <i class=" fa fa-suitcase"></i> Profile</a>
                             <a href="auth/logout" class="allright"> <i class="fa fa-key"></i> Log Out</a>
                     </div>
-                    <ul class="nav top-menu">
-                        </ul>
-                        <div class="sidebar-toggle-box">
-                            <div data-original-title="Toggle Navigation" data-placement="left" class="fa fa-bars tooltips"></div>
-                        </div>
-                        <!--logo start-->
-                        <div class="col-md-2 logo_bac">
-                            <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
-                            <!--logo end-->
-                        </div>
+                    <!--logo start-->
+                    <div class="col-md-2 logo_bac">
+                        <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
+                        <!--logo end-->
                     </div>
-             
+                    
+                    <div class="sidebar-toggle-box">
+                        <div data-original-title="Toggle Navigation" data-placement="left" class="fa fa-bars tooltips"></div>
+                    </div>
+                </div>
             </header>
             <!--header end-->
             <!--sidebar start-->
