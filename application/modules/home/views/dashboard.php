d7a1d1c7ce69129ae75bfc13172651fde004fe3a
askar <mr.askar1984@gmail.com>
2022-11-12 02:34:57 +0200

    r

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -11,7 +11,7 @@
         <title><?php echo $this->router->fetch_class(); ?> | Election Campaign Management System</title>
         <!-- Bootstrap core CSS -->
         <link href="common/css/bootstrap.min.css" rel="stylesheet">
-        <link href="common/css/bootstrap-reset.css" rel="stylesheet">
+        <link href="common/css/bootstrap-reset.css" rel="stylesheet">   
         <!--external css-->
         <link href="common/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
         <link rel="stylesheet" href="common/assets/data-tables/DT_bootstrap.css" />
@@ -39,7 +39,7 @@
                         <div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
                     </div>
                     <!--logo start-->
-                    <a href="" class="logo bold">ECMS <i class="fa fa-bullhorn"></i><span> </span></a>
+                    <a href="" class="logo bold">F O D A<span> </span>2025</a>
                     <!--logo end-->
                 </div>
                 <div class="nav notify-row" id="top_menu">
@@ -57,9 +57,9 @@
                     <ul class="nav pull-right top-menu">
                     </ul>
                     <div class=" col-md-3 pull-right padding_allright">
-                        <a href="settings" class="allright"> <i class="fa fa-cog"></i> Settings</a>
-                        <a href="profile" class="allright"> <i class=" fa fa-suitcase"></i> Profile</a>
-                        <a href="auth/logout" class="allright"> <i class="fa fa-key"></i> Log Out</a>
+                        <a href="settings" class="allright"> <i class="fa fa-cog"></i> الإعدادات</a>
+                        <a href="profile" class="allright"> <i class=" fa fa-suitcase"></i> الملف الشخصي</a>
+                        <a href="auth/logout" class="allright"> <i class="fa fa-key"></i> تسجيل الخروج</a>
                     </div>
                 </div>
             </header>
@@ -72,84 +72,84 @@
                         <li>
                             <a href="">
                                 <i class="fa fa-dashboard"></i>
-                                <span>Dashboard</span>
+                                <span>لوحة التحكم</span>
                             </a>
                         </li>
                         <?php if ($this->ion_auth->in_group('admin')) { ?>
                             <li>
                                 <a href="area">
                                     <i class="fa fa-home"></i>
-                                    <span>Area List</span>
+                                    <span>قائمة المناطق</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="volunteer" >
                                     <i class="fa fa-users"></i>
-                                    <span>Volunteer Management</span>
+                                    <span>إدارة الوكلاء</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="voter" >
                                     <i class="fa fa-user"></i>
-                                    <span>Voter Database</span>
+                                    <span>قاعدة بيانات الناخبين</span>
                                 </a>
                             </li>
                             <li class="sub-menu">
                                 <a href="javascript:;" >
                                     <i class="fa  fa-users"></i>
-                                    <span>Team</span>
+                                    <span>الفريق</span>
                                 </a>
                                 <ul class="sub">
-                                    <li><a  href="team"><i class="fa fa-users"></i>All Teams</a></li>
-                                    <li><a  href="team/addNewView"><i class="fa fa-plus"></i>Add Team</a></li>
+                                    <li><a  href="team"><i class="fa fa-users"></i>جميع الفرق </a></li>
+                                    <li><a  href="team/addNewView"><i class="fa fa-plus"></i>إضافة فريق</a></li>
                                 </ul>
                             </li>
                             <li>
                                 <a href="event" >
                                     <i class="fa fa-hand-o-up"></i>
-                                    <span>Event</span>
+                                    <span>الحدث</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="snw" >
                                     <i class="fa fa-strikethrough"></i>
-                                    <span>Campaign Analysis</span>
+                                    <span>تقييم الحملة</span>
                                 </a>
                             </li>
                             <li class="sub-menu">
                                 <a href="javascript:;" >
                                     <i class="fa  fa-money"></i>
-                                    <span>Expenses</span>
+                                    <span>التكاليف</span>
                                 </a>
                                 <ul class="sub">
-                                    <li><a  href="finance/expense"><i class="fa fa-money"></i>Expenses</a></li>
-                                    <li><a  href="finance/addExpenseView"><i class="fa fa-plus"></i>Add Expense</a></li>
-                                    <li><a  href="finance/expenseCategory"><i class="fa fa-credit-card"></i>Epense Categories</a></li>
-                                    <li><a  href="finance/addExpenseCategory"><i class="fa fa-plus"></i>Add Expense Category</a></li>
-                                    <li><a  href="finance/financialReport"><i class="fa fa-money"></i>Expense Report</a></li>
+                                    <li><a  href="finance/expense"><i class="fa fa-money"></i>المصروفات</a></li>
+                                    <li><a  href="finance/addExpenseView"><i class="fa fa-plus"></i>إضافة مصروف</a></li>
+                                    <li><a  href="finance/expenseCategory"><i class="fa fa-credit-card"></i>فئات المصروفات</a></li>
+                                    <li><a  href="finance/addExpenseCategory"><i class="fa fa-plus"></i>إضافة فئة مصروف</a></li>
+                                    <li><a  href="finance/financialReport"><i class="fa fa-money"></i>تقرير المصروفات</a></li>
                                 </ul>
                             </li>
                             <li class="sub-menu">
                                 <a href="javascript:;" >
                                     <i class="fa fa-envelope-o"></i>
-                                    <span>SMS</span>
+                                    <span>الرسائل النصية</span>
                                 </a>
                                 <ul class="sub">
-                                    <li><a  href="sms/sendView"><i class="fa fa-location-arrow"></i>Send SMS</a></li>
-                                    <li><a  href="sms/settings"><i class="fa fa-gear"></i>SMS Settings</a></li>
+                                    <li><a  href="sms/sendView"><i class="fa fa-location-arrow"></i>إرسال رسالة</a></li>
+                                    <li><a  href="sms/settings"><i class="fa fa-gear"></i>إعدادات الرسائل</a></li>
                                 </ul>
                             </li>  
                             <li>
                                 <a href="settings" >
                                     <i class="fa fa-gears"></i>
-                                    <span> Settings </span>
+                                    <span> الإعدادات </span>
                                 </a>
                             </li>
 
                             <li>
                                 <a href="profile" >
                                     <i class="fa fa-user"></i>
-                                    <span> Profile </span>
+                                    <span> الملف الشخصي </span>
                                 </a>
                             </li>
                         <?php } ?>
