e093124ccc91ebccae6e86aaadf295fb110bd027
Ascoor <94120646+Ascoor@users.noreply.github.com>
2025-08-02 21:36:33 +0300

    Implement Area module CRUD and tests

--- backend/app/Http/Resources/AreaResource.php
+++ backend/app/Http/Resources/AreaResource.php
@@ -8,6 +8,14 @@ class AreaResource extends JsonResource
 {
     public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'x' => $this->x,
+            'y' => $this->y,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
--- backend/database/seeders/AreaSeeder.php
+++ backend/database/seeders/AreaSeeder.php
@@ -2,12 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Area;
 use Illuminate\Database\Seeder;
 
 class AreaSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        Area::create([
+            'name' => 'Area 1',
+            'description' => 'Florida',
+            'x' => '',
+            'y' => '',
+        ]);
+
+        Area::create([
+            'name' => 'Area 2',
+            'description' => 'California',
+            'x' => '',
+            'y' => '',
+        ]);
     }
 }
