ffb72faff8cecb9d24f73fe9100078161411108d
Ascoor <94120646+Ascoor@users.noreply.github.com>
2025-08-02 22:14:38 +0300

    Add seeders for users and auth demo data

--- backend/database/seeders/AuthSeeder.php
+++ backend/database/seeders/AuthSeeder.php
@@ -2,12 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Auth;
 use Illuminate\Database\Seeder;
 
 class AuthSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        Auth::create();
     }
 }
