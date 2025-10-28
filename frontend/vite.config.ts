47263a5f01b87557c8cc44001f39229e2219a89d
Ascoor <mr.askar1984@gmail.com>
2025-10-28 08:35:40 +0300

    gh

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      
       "@app": path.resolve(__dirname, "./src/app"),
       "@features": path.resolve(__dirname, "./src/features"),
       "@shared": path.resolve(__dirname, "./src/shared"),
